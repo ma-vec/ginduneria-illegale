a98d870198d041dbaf72e3930d697489cd33b86d
ma-vec <maurov2006@gmail.com>
2026-02-20 20:00:28 +0100

    page bottle, JS for query string

---
+++ js/script-bottle.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const params = new URLSearchParams(window.location.search);
+    const ginId = params.get("id");
+
+    const nome = document.getElementById("nome-bottiglia");
+    const pos = document.getElementById("pos");
+    const botanic = document.getElementById("botanic");
+    const aggettivi = document.getElementById("aggettivi");
+    const bottleImage = document.getElementById("bottle-image");
+
+    async function LoadBottle() {
+
+        if (!ginId) {
+            alert("ID bottiglia non fornito nella URL");
+            return;
+        }
+
+        try {
+            const response = await fetch("/data/gin_list.json");
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            const ginObj = data.find(g => g.id === ginId);
+
+            if (!ginObj) {
+                alert("Bottiglia non trovata");
+                return;
+            }
+
+            nome.textContent = ginObj.nome;
+            pos.textContent = ginObj.provenienza;
+            bottleImage.src = `assets/images/${ginObj.id}.png`;
+
+            botanic.textContent = ginObj.botaniche.join(", ");
+            aggettivi.textContent = ginObj.aggettivi.join(", ");
+
+            bottleImage.alt = `Immagine di ${ginObj.nome}`;
+
+        } catch (err) {
+            console.error(err);
+            alert("Errore nel caricamento: " + err.message);
+        }
+    }
+
+    LoadBottle();
+});
